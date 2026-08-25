aa96301302c4468cbb8741e7f37dd41c87247c11
bhuvanshu <bhuvanshusingh@gmail.com>
2026-08-25 22:09:10 +0530

    chore:initialize repository

---
+++ src/test/java/com/homas/hospital_ms/HospitalMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homas.hospital_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
